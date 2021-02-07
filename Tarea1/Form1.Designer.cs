0d51e350fc29b82a8af445db9e2bd72ac388644b
andreanr19 <andrea.nr2000@gmail.com>
2021-02-07 02:50:45 +0100

    fixed something in the interface

--- Tarea1/Form1.Designer.cs
+++ Tarea1/Form1.Designer.cs
@@ -45,6 +45,7 @@ namespace Tarea1
             this.label3 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataTownGridRecord)).BeginInit();
             this.SuspendLayout();
             // 
@@ -180,11 +181,21 @@ namespace Tarea1
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
@@ -221,6 +232,7 @@ namespace Tarea1
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Tarea1/Form1.cs
+++ Tarea1/Form1.cs
@@ -151,5 +151,10 @@ namespace Tarea1
 
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
